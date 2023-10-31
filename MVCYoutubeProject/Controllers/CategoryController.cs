72076923688315e55f359251045390c94488824b
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-10-31 15:17:05 +0300

    I created crud views on Category page

--- MVCYoutubeProject/Controllers/CategoryController.cs
+++ MVCYoutubeProject/Controllers/CategoryController.cs
@@ -17,4 +17,95 @@ public class CategoryController : Controller
         IEnumerable<Category> objCategoryList = _db.Categories;
         return View(objCategoryList);
     }
+    //GET
+    public IActionResult Create()
+    {
+        return View();
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the same");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created succesfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+
+    }
+    public IActionResult Edit(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the same");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+
+            TempData["success"] = "Category updated succesfully";
+            return RedirectToAction("Index");
+        }
+
+        return View();
+
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted succesfully";
+        return RedirectToAction("Index");
+    }
 }
